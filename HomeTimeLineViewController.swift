0fc53f19a7b4358f3a889217f3f8aed4c679eaaa
Will Richman <wrichman@gmail.com>
2014-10-06 10:34:28 -0700

    Initial commit

---
+++ HomeTimeLineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeTimeLineViewController.swift
+//  TwitterClone
+//
+//  Created by William Richman on 10/6/14.
+//  Copyright (c) 2014 Will Richman. All rights reserved.
+//
+
+import UIKit
+
+class HomeTimeLineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
