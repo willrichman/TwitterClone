c1afe25a777a2d195e6dcd511eede09a777fabe6
Will Richman <wrichman@gmail.com>
2014-10-10 15:22:29 -0700

    Implemented the imageCache on the SingleTweetViewController

--- TwitterClone/SingleTweetViewController.swift
+++ TwitterClone/SingleTweetViewController.swift
@@ -18,6 +18,7 @@ class SingleTweetViewController: UIViewController {
     @IBOutlet weak var singleTweetUserName: UILabel!
     @IBOutlet weak var singleTweetProfileImage: UIImageView!
     var tweetShown: Tweet?
+    var imageCache = [String: UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,13 +36,31 @@ class SingleTweetViewController: UIViewController {
         self.singleTweetUserName.text = self.tweetShown!.profileName
         self.singleTweetTwitterHandle.text = ("@\(self.tweetShown!.screenName)")
         self.singleTweetText.text = self.tweetShown!.text
-        self.singleTweetProfileImage.image = self.tweetShown!.profileImage
         self.singleTweetFavoritesCount.text = String(self.tweetShown!.favoriteCount)
         self.singleTweetRTsCount.text = String(self.tweetShown!.retweetCount)
+        
+        if let cachedProfileImage = self.imageCache[tweetShown!.screenName] {
+            self.singleTweetProfileImage?.image = cachedProfileImage
+        }
+        else {
+            NetworkController.controller.fetchProfileImage(tweetShown!, completionHandler: { (errorDescription, tweetProfileImage) -> Void in
+                if errorDescription == nil {
+                    self.imageCache[self.tweetShown!.screenName] = tweetProfileImage
+                    self.singleTweetProfileImage?.image = self.imageCache[self.tweetShown!.screenName]
+                }
+                else {
+                    println("Error: \(errorDescription)")
+                }
+            })
+            
+        }
+
         self.singleTweetProfileImage.layer.borderColor = UIColor.whiteColor().CGColor;
         self.singleTweetProfileImage.layer.borderWidth = self.singleTweetProfileImage.frame.size.width * 0.05
         self.singleTweetProfileImage.layer.cornerRadius = self.singleTweetProfileImage.frame.size.width * 0.1
         self.singleTweetProfileImage.clipsToBounds = true
+        
+        
         
         let press = UITapGestureRecognizer(target: self, action: Selector("segueAction:"))
         self.singleTweetProfileImage.addGestureRecognizer(press)
